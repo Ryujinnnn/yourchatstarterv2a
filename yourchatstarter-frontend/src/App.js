77563482dad950a655f5e1c389bf35a2cc75152e
NeroYuki <kingrfminecraft@gmail.com>
2021-04-15 00:31:33 +0700

    more modify metadata

--- yourchatstarter-frontend/src/App.js
+++ yourchatstarter-frontend/src/App.js
@@ -12,25 +12,25 @@ class App extends Component {
       <div className="App" style={{backgroundColor: "#c27ac2"}}>
           <Helmet>
             {/*<!-- HTML Meta Tags -->*/}
-            <title>YourChatStarter - Your friendly assistant chatbot</title>
-            <meta name="description" content="A site where you can have our chatbot provide you with all the infomation you need in real life"/>
+            <title>YourChatStarter - Your informational chatbot</title>
+            <meta name="description" content="Have a chatbot to answer your everyday question today"/>
 
             {/* <!-- Google / Search Engine Tags --> */}
-            <meta itemprop="name" content="YourChatStarter - Your friendly assistant chatbot"/>
-            <meta itemprop="description" content="A site where you can have our chatbot provide you with all the infomation you need in real life"/>
+            <meta itemprop="name" content="YourChatStarter - Your informational chatbot"/>
+            <meta itemprop="description" content="Have a chatbot to answer your everyday question today"/>
             <meta itemprop="image" content=""/>
 
             {/* <!-- Facebook Meta Tags --> */}
             <meta property="og:url" content="http://www.yourchatstarter.tk"/>
             <meta property="og:type" content="website"/>
-            <meta property="og:title" content="YourChatStarter - Your friendly assistant chatbot"/>
-            <meta property="og:description" content="A site where you can have our chatbot provide you with all the infomation you need in real life"/>
+            <meta property="og:title" content="YourChatStarter - Your informational chatbot"/>
+            <meta property="og:description" content="Have a chatbot to answer your everyday question today"/>
             <meta property="og:image" content=""/>
 
             {/* <!-- Twitter Meta Tags --> */}
             <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:title" content="YourChatStarter - Your friendly assistant chatbot"/>
-            <meta name="twitter:description" content="A site where you can have our chatbot provide you with all the infomation you need in real life"/>
+            <meta name="twitter:title" content="YourChatStarter - Your informational chatbot"/>
+            <meta name="twitter:description" content="Have a chatbot to answer your everyday question today"/>
             <meta name="twitter:image" content=""/>
           </Helmet>
           <Header></Header>
