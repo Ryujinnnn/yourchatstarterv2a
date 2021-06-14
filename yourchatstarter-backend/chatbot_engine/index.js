ffd4a0f8ce2a8720b24fbd1f4f2040e86965ae79
NeroYuki <kingrfminecraft@gmail.com>
2021-06-14 16:11:51 +0700

    fix message size bug

--- yourchatstarter-backend/chatbot_engine/index.js
+++ yourchatstarter-backend/chatbot_engine/index.js
@@ -22,37 +22,44 @@ const uncertainResponsePool = [
 
 module.exports.get_response = function get_response(input, option = {}, context = {}) {
     return new Promise(async (resolve, reject) => {
-        let parsed_data = await wit_client.parse(input)
-
         let response = ""
-        //check for intent
-        let uncertain_flag = false
-        if (parsed_data.intents.length != 0) {
-            if (parsed_data.intents[0].confidence < 0.8) uncertain_flag = true
-            else {
-                let intent = IntentHandler.get(parsed_data.intents[0].name)
-                if (intent) {
-                    let entities = parsed_data.entities;
-                    [response, context] = await intent.run(entities, option, context)
-                }
-                else {
-                    response = "Chức năng chưa được xây dựng"
-                }
-            }
+        if (!input) {
+            response = "Tin nhắn của bạn quá ngắn, hãy gõ thêm gì đó vào nhé =)"
         }
-        else if (context.active_context.length != 0) {
-            [response, context] = await context_handling(parsed_data, input, option, context, IntentHandler)
+        else if (input.length > 270) {
+            response = "Tin nhắn của bạn quá dài, hãy cố gõ dưới 270 kí tự thôi nhé =)"
         }
         else {
-            //TODO: resolve description into vietnamese, somehow
-            await wiki_query(parsed_data.text).then(wiki_res => {
-                response = wiki_res[0].label + " là " + wiki_res[0].description 
-            })
-            .catch(() => {
-                response = notFoundResponsePool[random_index(notFoundResponsePool.length)]
-            })
+            let parsed_data = await wit_client.parse(input)
+            //check for intent
+            let uncertain_flag = false
+            if (parsed_data.intents.length != 0) {
+                if (parsed_data.intents[0].confidence < 0.8) uncertain_flag = true
+                else {
+                    let intent = IntentHandler.get(parsed_data.intents[0].name)
+                    if (intent) {
+                        let entities = parsed_data.entities;
+                        [response, context] = await intent.run(entities, option, context)
+                    }
+                    else {
+                        response = "Chức năng chưa được xây dựng"
+                    }
+                }
+            }
+            else if (context.active_context.length != 0) {
+                [response, context] = await context_handling(parsed_data, input, option, context, IntentHandler)
+            }
+            else {
+                //TODO: resolve description into vietnamese, somehow
+                await wiki_query(parsed_data.text).then(wiki_res => {
+                    response = wiki_res[0].label + " là " + wiki_res[0].description 
+                })
+                .catch(() => {
+                    response = notFoundResponsePool[random_index(notFoundResponsePool.length)]
+                })
+            }
+            if (!response && uncertain_flag) response = uncertainResponsePool[random_index(uncertainResponsePool.length)]
         }
-        if (!response && uncertain_flag) response = uncertainResponsePool[random_index(uncertainResponsePool.length)]
 
         context.past_client_message.push(input)
         if (context.past_client_message.length > 20) context.past_client_message.pop()
