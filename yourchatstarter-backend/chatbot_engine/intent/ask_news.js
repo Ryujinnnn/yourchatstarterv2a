9ba0cc5034acbee06dfe5adb44873e6b5239add1
NeroYuki <kingrfminecraft@gmail.com>
2021-06-29 18:15:08 +0700

    a quick fix and attempt on fixing locale time

--- yourchatstarter-backend/chatbot_engine/intent/ask_news.js
+++ yourchatstarter-backend/chatbot_engine/intent/ask_news.js
@@ -7,7 +7,7 @@ module.exports.run = (entities, option, context) => {
         console.log("fetching news")
         await get_news()
             .then(
-            (news_res) => {response = `[${new Date(news_res.created_at).toLocaleString()}] ${news_res.title}: ${news_res.desc} (Báo VNExpress)`},
+            (news_res) => {response = `[${new Date(news_res.created_at).toLocaleString('vi-VN', {timeZone: "Asia/Ho_Chi_Minh"})}] ${news_res.title}: ${news_res.desc} (Báo VNExpress)`},
             (e) => response = `Hôm nay mình quên mua báo rồi bạn ơi :(`)
         resolve([response, context])
     })
--- yourchatstarter-backend/chatbot_engine/intent/covid_recommend.js
+++ yourchatstarter-backend/chatbot_engine/intent/covid_recommend.js
@@ -1,7 +1,7 @@
 const random_helper = require("../utils/random_helper")
 
 module.exports.run = (entities, option, context) => {
-    return "Các bạn hãy tuân thủ quy tắc 5K: Khẩu trang, khoảng cách, không tập trung, khai báo và khử khuẩn nhé :D"
+    return ["Các bạn hãy tuân thủ quy tắc 5K: Khẩu trang, khoảng cách, không tập trung, khai báo và khử khuẩn nhé :D", context]
 }
 
 module.exports.name = "covid_recommend"
