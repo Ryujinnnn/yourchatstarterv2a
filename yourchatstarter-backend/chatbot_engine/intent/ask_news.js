fb1598e17da3d8df3278291b2d6b72590c836c69
NeroYuki <kingrfminecraft@gmail.com>
2021-12-04 23:13:00 +0700

    hotfix ask news intent

--- yourchatstarter-backend/chatbot_engine/intent/ask_news.js
+++ yourchatstarter-backend/chatbot_engine/intent/ask_news.js
@@ -6,15 +6,27 @@ module.exports.run = (entities, option, context, isLocal = false) => {
         let response = ""
         let category = ""
         context.active_context = []
-        if (entities['news_category:news_category']) category = entities['news_category:news_category'][0].value
-        console.log("fetching news")
-        await get_news(category)
-            .then(
+        if (isLocal) {
+            let news_category_entity = entities.find((val) => val.entity === "news_category")
+            if (news_category_entity) category = news_category_entity.option
+            await get_news(category).then(
                 (news_res) => {
                     response = `[${new Date(news_res.created_at).toLocaleString('vi-VN')}] ${news_res.title}: ${news_res.desc} [${news_res.article_link} - Đọc thêm tại VNExpress/]. Bạn muốn đọc tin tức nào khác không?`
                     context.suggestion_list = ['Quốc tế', 'Khoa học', 'Sức khỏe', 'Không, mình cảm ơn']
                 },
-                (e) => response = `Hôm nay mình quên mua báo rồi bạn ơi :(`)
+                (e) => response = `Hôm nay mình quên mua báo rồi bạn ơi :(`
+            )
+        }
+        else {
+            if (entities['news_category:news_category']) category = entities['news_category:news_category'][0].value
+            await get_news(category).then(
+                (news_res) => {
+                    response = `[${new Date(news_res.created_at).toLocaleString('vi-VN')}] ${news_res.title}: ${news_res.desc} [${news_res.article_link} - Đọc thêm tại VNExpress/]. Bạn muốn đọc tin tức nào khác không?`
+                    context.suggestion_list = ['Quốc tế', 'Khoa học', 'Sức khỏe', 'Không, mình cảm ơn']
+                },
+                (e) => response = `Hôm nay mình quên mua báo rồi bạn ơi :(`
+            )
+        }
         resolve([response, context])
     })
 }
