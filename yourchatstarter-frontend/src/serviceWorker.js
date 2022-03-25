9354a481c11a7dcc0b740cb2eecdd9c38aba1b29
NeroYuki <kingrfminecraft@gmail.com>
2022-03-25 10:57:13 +0700

    update service worker

--- yourchatstarter-frontend/src/serviceWorker.js
+++ yourchatstarter-frontend/src/serviceWorker.js
@@ -70,6 +70,25 @@ export function register(config) {
       }
     });
   }
+  else if ('serviceWorker' in navigator) {
+    // The URL constructor is available in all browsers that support SW.
+    console.log('public url', process.env.PUBLIC_URL)
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      console.log('invalid public url, aborting')
+      return;
+    }
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      console.log("window loading, will try to register a service worker at", swUrl)
+      console.log('trying to register new notification subscriber')
+      registerValidSW(swUrl, config);
+    })
+  }
 }
 
 function registerValidSW(swUrl, config) {
