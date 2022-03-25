42c8a66889bcc0cd80e0fa81567e4c527243b946
NeroYuki <kingrfminecraft@gmail.com>
2022-03-25 10:29:30 +0700

    add script for DO instance

--- yourchatstarter-frontend/src/serviceWorker.js
+++ yourchatstarter-frontend/src/serviceWorker.js
@@ -37,6 +37,7 @@ export function register(config) {
   console.log('attempt registering service worker')
   if (process.env.NODE_ENV !== 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    console.log('public url', process.env.PUBLIC_URL)
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
