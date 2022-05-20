69c1cc3c269442e3a3ec52ec0593e4cb4ede43d3
NeroYuki <kingrfminecraft@gmail.com>
2022-05-20 11:23:35 +0700

    hotfix sentiment analysis testing endpoint

--- yourchatstarter-backend/chatbot_engine/external_service/sentiment_analysist.js
+++ yourchatstarter-backend/chatbot_engine/external_service/sentiment_analysist.js
@@ -4,7 +4,7 @@ const axios = require('axios').default;
 
 module.exports.get_sentiment = function get_sentiment(input) {
     return new Promise((resolve, reject) => {
-        axios.post('localhost:8000/classify', {text: input}, err => {
+        axios.post('http://localhost:8000/classify', {text: input}, err => {
             console.log(err)
             reject("error in sentiment analysis server")
         }).then(res => {
